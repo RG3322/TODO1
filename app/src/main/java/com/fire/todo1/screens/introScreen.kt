ae261fb0d787eb276b16c5b196397f24ae006301
RG3322 <raghavsankhyaan@gmail.com>
2025-10-28 18:01:58 +0530

    initial commit

---
+++ app/src/main/java/com/fire/todo1/screens/introScreen.kt
@@ -0,0 +1,10 @@
+package com.fire.todo1.screens
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun IntroScreen(modifier: Modifier = Modifier){
+
+}
