83a2341991febd03759f55e02b9f5c81132ae456
RG3322 <raghavsankhyaan@gmail.com>
2025-10-28 20:34:54 +0530

    initial commit

--- app/src/main/java/com/fire/todo1/screens/introScreen.kt
+++ app/src/main/java/com/fire/todo1/screens/introScreen.kt
@@ -1,10 +1,32 @@
 package com.fire.todo1.screens
 
+import androidx.compose.ui.text.style.TextAlign
+import com.fire.todo1.R
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 
+@Preview(showBackground = true)
 @Composable
-fun IntroScreen(modifier: Modifier = Modifier){
+fun IntroScreen(){
+
+
+    Column (
+        modifier = Modifier.fillMaxSize().background(color = Color.DarkGray),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ){
+        Text(text="A step at time", color = Color.Black, fontSize = 30.sp)
+    }
 
 }
